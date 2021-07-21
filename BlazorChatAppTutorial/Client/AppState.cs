a028d9485c8d9912005812781a4f557644e84859
Michael Bond <codemonkey8510@icloud.com>
2021-07-21 07:22:17 -0400

    Add app state tracking

---
+++ BlazorChatAppTutorial/Client/AppState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorChatAppTutorial.Client
+{
+    public class AppState
+    {
+        public string UserName { get; set; }
+
+        public List<string> RoomNames { get; set; } = new List<string>();
+    }
+}
---
+++ BlazorChatAppTutorial/Client/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorChatAppTutorial.Client.Pages
+{
+    public partial class Index
+    {
+        private readonly AppState appState = new();
+
+        protected override void OnInitialized()
+        {
+            appState.UserName = AppState.UserName;
+            appState.RoomNames.AddRange(AppState.RoomNames);
+        }
+
+        private void OnValidFormSubmit()
+        {
+            AppState.UserName = appState.UserName;
+            AppState.RoomNames.Clear();
+            AppState.RoomNames.AddRange(appState.RoomNames);
+        }
+    }
+}
